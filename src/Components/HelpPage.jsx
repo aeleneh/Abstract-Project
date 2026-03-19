0e5da3a50c9aea9f134ec314e628acebb6d75ccc
Wunmi <enahoroomowunmi@gmail.com>
2026-03-19 20:40:12 +0100

    Added the touch manipulation and min-w-[44px] to make the arrow button responsive on mobile screens

--- src/Components/HelpPage.jsx
+++ src/Components/HelpPage.jsx
@@ -29,7 +29,7 @@ const HelpPage = () => {
     onKeyDown={handleKeyDown}
     className='text-base md:text-md border-[1px] border-black rounded-md mb-4 py-4
     md:py-6 pl-4 pr-12 w-full outline-none shadow-xl font-light'/>
-    <div className='absolute inset-y-0 right-0 flex items-center pr-4 cursor-pointer'
+    <div className='absolute inset-y-0 right-0 flex items-center pr-4 cursor-pointer touch-manipulation min-w-[44px]'
     onclick={() => handleSearch()}>
     <ArrowRight className='w-5 h-5 md:w-6 md:h-6  text-black'/>
     </div>
